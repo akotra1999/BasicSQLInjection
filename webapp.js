e9328fdb01790d3681e60f12c3c4a10cecbbc210
unknown <akotra1999@gmail.com>
2021-07-21 10:04:43 -0700

    Connect website to database

--- webapp.js
+++ webapp.js
@@ -1,5 +1,8 @@
 let http = require("http")
 let fs = require('fs');
+let qs = require('querystring');
+let mysql = require('mysql');
+require("dotenv").config();
 
 http.createServer(onRequest).listen(8888);
 console.log("Server has started");
@@ -7,20 +10,66 @@ console.log("Server has started");
 function onRequest(request, response) {
 
   if (request.method === "GET") {
+
     response.writeHead(200, {
-      'Content-Type': 'text/html'
+      "Content-Type": "text/html"
     });
-    fs.readFile('./index.html', null, function(error, data) {
+
+    fs.readFile("./index.html", null, function(error, data) {
+
       if (error) {
         response.writeHead(404);
-        respone.write('Whoops! File not found!');
+        respone.write("Whoops! File not found!");
+
       } else {
         response.write(data);
       }
       response.end();
     });
+
   } else if (request.method === "POST") {
-    console.log("POST request");
-    response.end();
+    let body = "";
+
+    request.on("data", function(data) {
+      body += data;
+    });
+
+    request.on("end", function() {
+      let post = qs.parse(body);
+
+      let connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: process.env.PASSWORD,
+        database: "test_data"
+      });
+
+      let sql = "SELECT * FROM login_info WHERE username = '" +
+        post["username"] + "' AND " + "password = '" + post["password"] + "'";
+
+      connection.query(sql, (error, results, fields) => {
+
+        if (error) {
+          return console.error(error.message);
+        }
+
+        if(results.length != 0) {
+          response.writeHead(200);
+          response.write("Success");
+        } else {
+          response.write("Invalid login");
+        }
+        response.end();
+      });
+
+      connection.end(function(err) {
+
+        if (err) {
+          return console.error("error: " + err.message);
+        }
+        console.log("Closing the database connection.");
+      });
+    });
+
   }
 }
